bfee5a68d3a545aae63f05853490081f1a50be68
Joshua M Loran <joshua.loran@gmail.com>
2019-01-28 16:42:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |seasons, attribute|
     if seasons == season
-      return attribute[0]
+      return attribute[0][0]
     end
   end
 end
